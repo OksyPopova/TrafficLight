a9c51c8de22f6d8a85924542d507e082d377eb64
OksyPopova <oxana12345616@gmail.com>
2022-09-18 23:55:00 +0300

    Traffic light settings

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redSignalView: UIView!
+    @IBOutlet var yellowSignalView: UIView!
+    @IBOutlet var greenSignalView: UIView!
+    @IBOutlet var swichButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redSignalView.layer.cornerRadius = 60
+        yellowSignalView.layer.cornerRadius = 60
+        greenSignalView.layer.cornerRadius = 60
+        
+        redSignalView.alpha = 0.3
+        yellowSignalView.alpha = 0.3
+        greenSignalView.alpha = 0.3
+        swichButton.layer.cornerRadius = 10
+        
+        
+        }
+    
+    @IBAction func buttonAction() {
+        
     }
-
-
+    
+   
 }
-
