28db38b58f3b2dbcfec8f708d947779118b88181
Оксана Попова <oxana12345616@gmail.com>
2022-09-21 22:20:49 +0300

    fix crash

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -14,25 +14,45 @@ class ViewController: UIViewController {
     @IBOutlet var greenSignalView: UIView!
     @IBOutlet var swichButton: UIButton!
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        redSignalView.layer.cornerRadius = 60
-        yellowSignalView.layer.cornerRadius = 60
-        greenSignalView.layer.cornerRadius = 60
-        
-        redSignalView.alpha = 0.3
-        yellowSignalView.alpha = 0.3
-        greenSignalView.alpha = 0.3
-        swichButton.layer.cornerRadius = 10
-        
-        
-        }
-    
-    @IBAction func buttonAction() {
-        
-    }
+    private var viewArray: [UIView?] = []
+      private var currentIndex = 0
+      
+      override func viewDidLoad() {
+          super.viewDidLoad()
+          
+          swichButton.setTitle("START", for: .normal)
+          
+          redSignalView.layer.cornerRadius = redSignalView.frame.width / 2
+          yellowSignalView.layer.cornerRadius = yellowSignalView.frame.width / 2
+          greenSignalView.layer.cornerRadius = greenSignalView.frame.width / 2
+          
+          redSignalView.alpha = 0.3
+          yellowSignalView.alpha = 0.3
+          greenSignalView.alpha = 0.3
+          swichButton.layer.cornerRadius = 10
+          
+          viewArray.append(redSignalView)
+          viewArray.append(yellowSignalView)
+          viewArray.append(greenSignalView)
+          
+      }
+      
+      
+      @IBAction func buttonAction(_ sender: UIButton) {
+          guard currentIndex >= 0 && currentIndex < viewArray.count  else{
+              return
+          }
+          for item in viewArray {
+              item?.alpha = 0.3
+          }
+          viewArray[currentIndex]?.alpha = 1.0
+          swichButton.setTitle("NEXT", for: .normal)
+          currentIndex += 1
+          
+          if currentIndex == viewArray.count {
+          currentIndex = 0
+          }
+      }
     
    
 }
