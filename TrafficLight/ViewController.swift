ab52f6e0e9ced5bbbd79d6e03189bff48e10e173
Оксана Попова <oxana12345616@gmail.com>
2022-09-21 22:00:28 +0300

    add space

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet var greenSignalView: UIView!
     @IBOutlet var swichButton: UIButton!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
